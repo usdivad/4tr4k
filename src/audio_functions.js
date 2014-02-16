61db606a7ccb2fdaeb76f99f36499c17aa293840
David Su <david.d.su@gmail.com>
2014-02-15 20:29:50 -0500

    snapshot functionality, clear button

--- src/audio_functions.js
+++ src/audio_functions.js
@@ -1,195 +1,205 @@
 function set_tracks(rh, rs, rk, rn, ph, ps, pk, pn, b) {
-				$("#hat_text").val(rh);
-				$("#snare_text").val(rs);
-				$("#kick_text").val(rk);
-				$("#noise_text").val(rn);
+	$("#hat_text").val(rh);
+	$("#snare_text").val(rs);
+	$("#kick_text").val(rk);
+	$("#noise_text").val(rn);
 
 
-				$("#h_seq_text").val(ph);
-				$("#s_seq_text").val(ps);
-				$("#k_seq_text").val(pk);
-				$("#n_seq_text").val(pn);
+	$("#h_seq_text").val(ph);
+	$("#s_seq_text").val(ps);
+	$("#k_seq_text").val(pk);
+	$("#n_seq_text").val(pn);
 
-				$("#bpm_text").val(b);
+	$("#bpm_text").val(b);
 
 
-				//toggle these on/off depending on whether you want them that way. e.g. 
-				/*
-				hat_index = 0;
-				snare_index = 0;
-				kick_index = 0;
-				noise_index = 0;
-				*/
+	//toggle these on/off depending on whether you want them that way. e.g. 
+	/*
+	hat_index = 0;
+	snare_index = 0;
+	kick_index = 0;
+	noise_index = 0;
+	*/
 
-				updateAll();
-			}
+	updateAll();
+}
 
-			function transform_tracks(rh, rs, rk, rn, ph, ps, pk, pn, b) {
-				//set gears in motion for iterative transformation
-				oldMelody = newMelody;
-				newMelody = [rh, rs, rk, rn, ph, ps, pk, pn, b];
-				ct_counter = 0;
-				
-				//update audio
-				//updateAll();
-			}
+function transform_tracks(rh, rs, rk, rn, ph, ps, pk, pn, b) {
+	//set gears in motion for iterative transformation
+	oldMelody = newMelody;
+	newMelody = [rh, rs, rk, rn, ph, ps, pk, pn, b];
+	ct_counter = 0;
+	
+	//update audio
+	//updateAll();
+}
 
-			//bpm to ms
-			function bpm_to_ms(b) {
-				return (1000/(b/60))/4; //4 because we're at 16thnotes, which is 1/4 of a quarter note (the bpm val)
-			}
-			function ms_to_bpm(m) {
-				return ((m/1000)*60);
-			}
+//bpm to ms
+function bpm_to_ms(b) {
+	return (1000/(b/60))/4; //4 because we're at 16thnotes, which is 1/4 of a quarter note (the bpm val)
+}
+function ms_to_bpm(m) {
+	return ((m/1000)*60);
+}
 
-			//poll/update
-			function updateAll() {
-				//rhythm
-				if ($("#hat_text").val()) {
-					hat_data = $("#hat_text").val();
-				}
-				if ($("#snare_text").val()) {
-					snare_data = $("#snare_text").val();
-				}
-				if ($("#kick_text").val()) {
-					kick_data = $("#kick_text").val();
-				}
-				if ($("#noise_text").val()) {
-					noise_data = $("#noise_text").val();
-				}
+//poll/update
+function updateAll() {
+	//rhythm
+	if ($("#hat_text").val()) {
+		hat_data = $("#hat_text").val();
+	}
+	if ($("#snare_text").val()) {
+		snare_data = $("#snare_text").val();
+	}
+	if ($("#kick_text").val()) {
+		kick_data = $("#kick_text").val();
+	}
+	if ($("#noise_text").val()) {
+		noise_data = $("#noise_text").val();
+	}
 
-				//pitch				
-				if ($("#h_seq_text").val() && $("#h_seq_text").val()!=h_seq_string) {
-					h_seq_string = $("#h_seq_text").val();
-					hat_sequence = T($("#h_seq_text").val().split(" ").map(atof));
-					hat_synth.freq = hat_sequence;
-					//hat_synth.freq.bang();
-				}
-				if ($("#s_seq_text").val() && $("#s_seq_text").val() != s_seq_string) {
-					s_seq_string = $("#s_seq_text").val();
-					snare_sequence = T($("#s_seq_text").val().split(" ").map(atof));
-					snare_synth.freq = snare_sequence;
-					//snare_synth.freq.bang();
-				}
-				if ($("#k_seq_text").val() && $("#k_seq_text").val() != k_seq_string) {
-					k_seq_string = $("#k_seq_text").val();
-					kick_sequence = T($("#k_seq_text").val().split(" ").map(atof));
-					kick_synth.freq = kick_sequence;
-					//kick_synth.freq.bang();
-				}
-				if ($("#n_seq_text").val() && $("#n_seq_text").val() != n_seq_string) {
-					n_seq_string = $("#n_seq_text").val();
-					noise_sequence = T($("#n_seq_text").val().split(" ").map(atof));
-					noise_synth.freq = noise_sequence;
-				}
-				
-				//glob
-				bpm = bpm_to_ms($("#bpm_text").val());
-				timer.interval = bpm;			
+	//pitch				
+	if ($("#h_seq_text").val() && $("#h_seq_text").val()!=h_seq_string) {
+		h_seq_string = $("#h_seq_text").val();
+		hat_sequence = T($("#h_seq_text").val().split(" ").map(atof));
+		hat_synth.freq = hat_sequence;
+		//hat_synth.freq.bang();
+	}
+	if ($("#s_seq_text").val() && $("#s_seq_text").val() != s_seq_string) {
+		s_seq_string = $("#s_seq_text").val();
+		snare_sequence = T($("#s_seq_text").val().split(" ").map(atof));
+		snare_synth.freq = snare_sequence;
+		//snare_synth.freq.bang();
+	}
+	if ($("#k_seq_text").val() && $("#k_seq_text").val() != k_seq_string) {
+		k_seq_string = $("#k_seq_text").val();
+		kick_sequence = T($("#k_seq_text").val().split(" ").map(atof));
+		kick_synth.freq = kick_sequence;
+		//kick_synth.freq.bang();
+	}
+	if ($("#n_seq_text").val() && $("#n_seq_text").val() != n_seq_string) {
+		n_seq_string = $("#n_seq_text").val();
+		noise_sequence = T($("#n_seq_text").val().split(" ").map(atof));
+		noise_synth.freq = noise_sequence;
+	}
+	
+	//glob
+	bpm = bpm_to_ms($("#bpm_text").val());
+	timer.interval = bpm;			
 
-			}
+}
 
-			//timer -> synth on or off (dep on code in)
-			function onOff(data, synth, i, seq, disp) {
+//timer -> synth on or off (dep on code in)
+function onOff(data, synth, i, seq, disp) {
 
-				//console.log(i);
-				if (data[i] == "1") {
-					synth.mul = amp;
-					seq.bang();
-					disp.css("visibility", "visible");
-				}
-				else if (data[i] == "X" || data[i] == "x") {
-					var dice = Math.random();
-					if (dice > 0.5) {
-						synth.mul = amp;
-						seq.bang();
-						disp.css("visibility", "visible");
-					}
-					else {
-						synth.mul = 0;
-						disp.css("visibility", "hidden");
-					}
-				}
-				else {
-					synth.mul = 0;
-					disp.css("visibility", "hidden");
-				}
-				if (i>=data.length-1) {
-					i = 0;
-				}
-				else {
-					i++;
-				}
-				return i;
-			}
+	//console.log(i);
+	if (data[i] == "1") {
+		synth.mul = amp;
+		seq.bang();
+		disp.css("visibility", "visible");
+	}
+	else if (data[i] == "X" || data[i] == "x") {
+		var dice = Math.random();
+		if (dice > 0.5) {
+			synth.mul = amp;
+			seq.bang();
+			disp.css("visibility", "visible");
+		}
+		else {
+			synth.mul = 0;
+			disp.css("visibility", "hidden");
+		}
+	}
+	else {
+		synth.mul = 0;
+		disp.css("visibility", "hidden");
+	}
+	if (i>=data.length-1) {
+		i = 0;
+	}
+	else {
+		i++;
+	}
+	return i;
+}
 
-			//play all
-			function playAll() {
-				hat_synth.play();
-				snare_synth.play();
-				kick_synth.play();
-				noise_synth.play();
-				timer.on();
-			}
+//play all
+function playAll() {
+	hat_synth.play();
+	snare_synth.play();
+	kick_synth.play();
+	noise_synth.play();
+	timer.on();
+}
 
-			//pause all
-			function pauseAll() {
-				hat_synth.pause();
-				snare_synth.pause();
-				kick_synth.pause();
-				noise_synth.pause();
-				timer.off();
-				hat_index = 0;
-				snare_index = 0;
-				kick_index = 0;
-				noise_index = 0;
-				hat_synth.freq = hat_sequence;
-				snare_synth.freq = snare_sequence;
-				kick_synth.freq = kick_sequence;
-				noise_synth.freq = noise_sequence;
+//pause all
+function pauseAll() {
+	hat_synth.pause();
+	snare_synth.pause();
+	kick_synth.pause();
+	noise_synth.pause();
+	timer.off();
+	hat_index = 0;
+	snare_index = 0;
+	kick_index = 0;
+	noise_index = 0;
+	hat_synth.freq = hat_sequence;
+	snare_synth.freq = snare_sequence;
+	kick_synth.freq = kick_sequence;
+	noise_synth.freq = noise_sequence;
 
-				/*
-				hat_synth.freq.index = 0;
-				snare_synth.freq.index = 0;
-				kick_synth.freq.index = 0;
-				*/
-			}
+	/*
+	hat_synth.freq.index = 0;
+	snare_synth.freq.index = 0;
+	kick_synth.freq.index = 0;
+	*/
+}
 
 
 //octave functions; only goes from 0-10
-			//you gotta press enter after to acutally update
-			function octDown(s, box) {
-				var ns = s;
-				ns = ns.replace(/1/g, "0");
-				ns = ns.replace(/2/g, "1");
-				ns = ns.replace(/3/g, "2");
-				//console.log(ns);
-				ns = ns.replace(/4/g,"3");
-				ns = ns.replace(/5/g, "4");
-				ns = ns.replace(/6/g, "5");
-				ns = ns.replace(/7/g,"6");
-				ns = ns.replace(/8/g, "7");
-				ns = ns.replace(/9/g, "8");
-				ns = ns.replace(/10/g,"9");
-				console.log(ns);
+//you gotta press enter after to acutally update
+function octDown(s, box) {
+	var ns = s;
+	ns = ns.replace(/1/g, "0");
+	ns = ns.replace(/2/g, "1");
+	ns = ns.replace(/3/g, "2");
+	//console.log(ns);
+	ns = ns.replace(/4/g,"3");
+	ns = ns.replace(/5/g, "4");
+	ns = ns.replace(/6/g, "5");
+	ns = ns.replace(/7/g,"6");
+	ns = ns.replace(/8/g, "7");
+	ns = ns.replace(/9/g, "8");
+	ns = ns.replace(/10/g,"9");
+	console.log(ns);
 
-				box.val(ns);
-			}
+	box.val(ns);
+}
 
-			function octUp(s, box) {
-				var ns = s;
-				ns = ns.replace(/9/g, "10");
-				ns = ns.replace(/8/g, "9");
-				//console.log(ns);
-				ns = ns.replace(/7/g,"8");
-				ns = ns.replace(/6/g, "7");
-				ns = ns.replace(/5/g, "6");
-				ns = ns.replace(/4/g,"5");
-				ns = ns.replace(/3/g, "4");
-				ns = ns.replace(/2/g, "3");
-				ns = ns.replace(/1/g,"2");
-				ns = ns.replace(/0/g,"1");
-				//console.log(ns);
+function octUp(s, box) {
+	var ns = s;
+	ns = ns.replace(/9/g, "10");
+	ns = ns.replace(/8/g, "9");
+	//console.log(ns);
+	ns = ns.replace(/7/g,"8");
+	ns = ns.replace(/6/g, "7");
+	ns = ns.replace(/5/g, "6");
+	ns = ns.replace(/4/g,"5");
+	ns = ns.replace(/3/g, "4");
+	ns = ns.replace(/2/g, "3");
+	ns = ns.replace(/1/g,"2");
+	ns = ns.replace(/0/g,"1");
+	//console.log(ns);
 
-				box.val(ns);
-			}
+	box.val(ns);
+}
+
+function snapshot_save() {
+	var snapshot = [$("#hat_text").val(), $("#snare_text").val(), $("#kick_text").val(), $("#noise_text").val(), $("#h_seq_text").val(), $("#s_seq_text").val(), $("#k_seq_text").val(), $("#n_seq_text").val(), $("#bpm_text").val(),];
+	return snapshot;
+}
+
+function snapshot_set(snapshot) {
+	var s = snapshot;
+	set_tracks(s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8]);
+}
--- src/input.js
+++ src/input.js
@@ -1,196 +1,222 @@
 function pianoInput(note) {
-				var chk = $('input[name="select"]:checked').val();
-				//var 
+	var chk = $('input[name="select"]:checked').val();
+	//var 
 
-				if (chk=="h") {
-					if ($("#h_seq_text").val() == "") {
-						$("#h_seq_text").val(note);
-					}
-					else {
-						$("#h_seq_text").val($("#h_seq_text").val()+" "+note);
-					}
-				}
-				else if (chk=="s") {
-					if ($("#s_seq_text").val() == "") {
-						$("#s_seq_text").val(note);
-					}
-					else {
-						$("#s_seq_text").val($("#s_seq_text").val()+" "+note);
-					}
-				}
-				else if (chk=="k") {
-					if ($("#k_seq_text").val() == "") {
-						$("#k_seq_text").val(note);
-					}
-					else {
-						$("#k_seq_text").val($("#k_seq_text").val()+" "+note);
-					}
-				}
-				else if (chk=="n") {
-					if ($("#n_seq_text").val() == "") {
-						$("#n_seq_text").val(note);
-					}
-					else {
-						$("#n_seq_text").val($("#n_seq_text").val()+" "+note);
-					}
-				}
+	if (chk=="h") {
+		if ($("#h_seq_text").val() == "") {
+			$("#h_seq_text").val(note);
+		}
+		else {
+			$("#h_seq_text").val($("#h_seq_text").val()+" "+note);
+		}
+	}
+	else if (chk=="s") {
+		if ($("#s_seq_text").val() == "") {
+			$("#s_seq_text").val(note);
+		}
+		else {
+			$("#s_seq_text").val($("#s_seq_text").val()+" "+note);
+		}
+	}
+	else if (chk=="k") {
+		if ($("#k_seq_text").val() == "") {
+			$("#k_seq_text").val(note);
+		}
+		else {
+			$("#k_seq_text").val($("#k_seq_text").val()+" "+note);
+		}
+	}
+	else if (chk=="n") {
+		if ($("#n_seq_text").val() == "") {
+			$("#n_seq_text").val(note);
+		}
+		else {
+			$("#n_seq_text").val($("#n_seq_text").val()+" "+note);
+		}
+	}
 
-				//updateAll();
-				console.log(note);
+	//updateAll();
+	console.log(note);
 
-			}
-
-
+}
 
 
-			/** GEO <--> MELODY **/
-			//incl. locations
-			var LOC_MARGIN = 0.005;
-			var EC_LAT = 40.807;
-			var EC_LON = -73.959;
 
-			function det_melody(lat, lon) {
-				//hard-coded positions
 
-				//EC
-				if (Math.abs(lat - EC_LAT) < LOC_MARGIN && Math.abs(lon - EC_LON) < LOC_MARGIN) {
-					dax = 1;
-				}
-				else {
-					dax = 0;
-				}
-			}
+/** GEO <--> MELODY **/
+//incl. locations
+var LOC_MARGIN = 0.005;
+var EC_LAT = 40.807;
+var EC_LON = -73.959;
+
+function det_melody(lat, lon) {
+	//hard-coded positions
+
+	//EC
+	if (Math.abs(lat - EC_LAT) < LOC_MARGIN && Math.abs(lon - EC_LON) < LOC_MARGIN) {
+		dax = 1;
+	}
+	else {
+		dax = 0;
+	}
+}
 
 //controls
-			//incl. focus shortcuts
-			//keyup so that  "focus" functions will happen after key is pressed
-			$("body").keyup(function(e) { 
+//incl. focus shortcuts
+//keyup so that  "focus" functions will happen after key is pressed
+$("body").keyup(function(e) { 
 
-				//var backspace = jQuery.Event("keydown", { keyCode: 20 });
+	//var backspace = jQuery.Event("keydown", { keyCode: 20 });
 
-				if (e.keyCode == 13) { //ENTER to updateAll
-					updateAll();
-				}
-				else if (e.keyCode == 85) { //U
-					var t = $("#hat_text").val();
-					$("#hat_text").focus();
-					$("#hat_text").val(t);
-				}
-				else if (e.keyCode == 73) { //I
-					var t = $("#snare_text").val();
-					$("#snare_text").focus();
-					$("#snare_text").val(t);
-				}
-				else if (e.keyCode == 79) { //o
-					var t = $("#kick_text").val();
-					$("#kick_text").focus();
-					$("#kick_text").val(t);
-				}
-				else if (e.keyCode == 80) { //P
-					var t = $("#noise_text").val();
-					$("#noise_text").focus();
-					$("#noise_text").val(t);
-				}
-				else if (e.keyCode == 74) { //J
-					var t = $("#h_seq_text").val();
-					$("#h_seq_text").focus();
-					$("#h_seq_text").val(t);
-					$("#h_select").attr("checked", "checked");
-				}
-				else if (e.keyCode == 75) { //K
-					var t = $("#s_seq_text").val();
-					$("#s_seq_text").focus();
-					$("#s_seq_text").val(t);
-					$("#s_select").attr("checked", "checked");
-				}
-				else if (e.keyCode == 76) { //L
-					var t = $("#k_seq_text").val();
-					$("#k_seq_text").focus();
-					$("#k_seq_text").val(t);
-					$("#k_select").attr("checked", "checked");
-				}
-				else if (e.keyCode == 186) { //;
-					var t = $("#n_seq_text").val();
-					$("#n_seq_text").focus();
-					$("#n_seq_text").val(t);
-					$("#n_select").attr("checked", "checked");
-				}
-			});
-			$("#play").click(function() {playAll();});
-			$("#pause").click(function() {pauseAll();});
-			$("#preset_gp0").click(function() {gp0();});
-			$("#preset_gp1").click(function() {gp1();});
-			$("#preset_gp2").click(function() {gp2();});
-			$("#preset_roar").click(function() {roar();});
-			$("#preset_randomScales").click(function() {randomScales();});
-			$("#preset_giveDrummer").click(function() {giveDrummer();});
-
-
-			//controls for keyboard shortcuts macros
-			$("#hat_text").keyup(function(e) {
-				if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
-					//this.value=this.value.replace(/[jkl;uiopr]/,'');
-					var t = $("#hat_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
-					$("#hat_text").val(t);
-				}
-			});
-			$("#snare_text").keyup(function(e) {
-				if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
-					//this.value=this.value.replace(/[jkl;uiopr]/,'');
-					var t = $("#snare_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
-					$("#snare_text").val(t);
-				}
-			});
-			$("#kick_text").keyup(function(e) {
-				if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
-					//this.value=this.value.replace(/[jkl;uiopr]/,'');
-					var t = $("#kick_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
-					$("#kick_text").val(t);
-				}
-			});
-			$("#noise_text").keyup(function(e) {
-				if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
-					//this.value=this.value.replace(/[jkl;uiopr]/,'');
-					var t = $("#noise_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
-					$("#noise_text").val(t);
-				}
-			});
-			$("#h_seq_text").keyup(function(e) {
-				if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
-					//this.value=this.value.replace(/[jkl;uiopr]/,'');
-					var t = $("#h_seq_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
-					$("#h_seq_text").val(t);
-				}
-			});
-			$("#s_seq_text").keyup(function(e) {
-				if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
-					//this.value=this.value.replace(/[jkl;uiopr]/,'');
-					var t = $("#s_seq_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
-					$("#s_seq_text").val(t);
-				}
-			});
-			$("#k_seq_text").keyup(function(e) {
-				if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
-					//this.value=this.value.replace(/[jkl;uiopr]/,'');
-					var t = $("#k_seq_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
-					$("#k_seq_text").val(t);
-				}
-			});
-			$("#n_seq_text").keyup(function(e) {
-				if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
-					//this.value=this.value.replace(/[jkl;uiopr]/,'');
-					var t = $("#n_seq_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
-					$("#n_seq_text").val(t);
-				}
-			});
+	if (e.keyCode == 13) { //ENTER to updateAll
+		updateAll();
+	}
+	else if (e.keyCode == 85) { //U
+		var t = $("#hat_text").val();
+		$("#hat_text").focus();
+		$("#hat_text").val(t);
+	}
+	else if (e.keyCode == 73) { //I
+		var t = $("#snare_text").val();
+		$("#snare_text").focus();
+		$("#snare_text").val(t);
+	}
+	else if (e.keyCode == 79) { //o
+		var t = $("#kick_text").val();
+		$("#kick_text").focus();
+		$("#kick_text").val(t);
+	}
+	else if (e.keyCode == 80) { //P
+		var t = $("#noise_text").val();
+		$("#noise_text").focus();
+		$("#noise_text").val(t);
+	}
+	else if (e.keyCode == 74) { //J
+		var t = $("#h_seq_text").val();
+		$("#h_seq_text").focus();
+		$("#h_seq_text").val(t);
+		$("#h_select").attr("checked", "checked");
+	}
+	else if (e.keyCode == 75) { //K
+		var t = $("#s_seq_text").val();
+		$("#s_seq_text").focus();
+		$("#s_seq_text").val(t);
+		$("#s_select").attr("checked", "checked");
+	}
+	else if (e.keyCode == 76) { //L
+		var t = $("#k_seq_text").val();
+		$("#k_seq_text").focus();
+		$("#k_seq_text").val(t);
+		$("#k_select").attr("checked", "checked");
+	}
+	else if (e.keyCode == 186) { //;
+		var t = $("#n_seq_text").val();
+		$("#n_seq_text").focus();
+		$("#n_seq_text").val(t);
+		$("#n_select").attr("checked", "checked");
+	}
+});
+$("#play").click(function() {playAll();});
+$("#pause").click(function() {pauseAll();});
+$("#clear").click(function() {
+	set_tracks("0","0","0","0","A3","A2","A1","A3",$("#bpm_text").val());
+});
+$("#preset_gp0").click(function() {gp0();});
+$("#preset_gp1").click(function() {gp1();});
+$("#preset_gp2").click(function() {gp2();});
+$("#preset_roar").click(function() {roar();});
+$("#preset_randomScales").click(function() {randomScales();});
+$("#preset_giveDrummer").click(function() {giveDrummer();});
 
 
-			//controls for octavebuttons
-			$("#h_octDown").click(function() {octDown($("#h_seq_text").val(), $("#h_seq_text")); updateAll();});
-			$("#h_octUp").click(function() {octUp($("#h_seq_text").val(), $("#h_seq_text")); updateAll();});
-			$("#s_octDown").click(function() {octDown($("#s_seq_text").val(), $("#s_seq_text")); updateAll();});
-			$("#s_octUp").click(function() {octUp($("#s_seq_text").val(), $("#s_seq_text")); updateAll();});
-			$("#k_octDown").click(function() {octDown($("#k_seq_text").val(), $("#k_seq_text")); updateAll();});
-			$("#k_octUp").click(function() {octUp($("#k_seq_text").val(), $("#k_seq_text")); updateAll();});
-			$("#n_octDown").click(function() {octDown($("#n_seq_text").val(), $("#n_seq_text")); updateAll();});
-			$("#n_octUp").click(function() {octUp($("#n_seq_text").val(), $("#n_seq_text")); updateAll();});
+//controls for keyboard shortcuts macros
+$("#hat_text").keyup(function(e) {
+	if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
+		//this.value=this.value.replace(/[jkl;uiopr]/,'');
+		var t = $("#hat_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
+		$("#hat_text").val(t);
+	}
+});
+$("#snare_text").keyup(function(e) {
+	if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
+		//this.value=this.value.replace(/[jkl;uiopr]/,'');
+		var t = $("#snare_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
+		$("#snare_text").val(t);
+	}
+});
+$("#kick_text").keyup(function(e) {
+	if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
+		//this.value=this.value.replace(/[jkl;uiopr]/,'');
+		var t = $("#kick_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
+		$("#kick_text").val(t);
+	}
+});
+$("#noise_text").keyup(function(e) {
+	if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
+		//this.value=this.value.replace(/[jkl;uiopr]/,'');
+		var t = $("#noise_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
+		$("#noise_text").val(t);
+	}
+});
+$("#h_seq_text").keyup(function(e) {
+	if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
+		//this.value=this.value.replace(/[jkl;uiopr]/,'');
+		var t = $("#h_seq_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
+		$("#h_seq_text").val(t);
+	}
+});
+$("#s_seq_text").keyup(function(e) {
+	if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
+		//this.value=this.value.replace(/[jkl;uiopr]/,'');
+		var t = $("#s_seq_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
+		$("#s_seq_text").val(t);
+	}
+});
+$("#k_seq_text").keyup(function(e) {
+	if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
+		//this.value=this.value.replace(/[jkl;uiopr]/,'');
+		var t = $("#k_seq_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
+		$("#k_seq_text").val(t);
+	}
+});
+$("#n_seq_text").keyup(function(e) {
+	if (e.keyCode == 85 || e.keyCode == 73 || e.keyCode == 79 || e.keyCode == 80 || e.keyCode == 74 || e.keyCode == 75 || e.keyCode == 76 || e.keyCode == 186) {
+		//this.value=this.value.replace(/[jkl;uiopr]/,'');
+		var t = $("#n_seq_text").val().replace(/[jkl;JKL;uioprUIOPR]/,"");
+		$("#n_seq_text").val(t);
+	}
+});
+
+
+//controls for octavebuttons
+$("#h_octDown").click(function() {octDown($("#h_seq_text").val(), $("#h_seq_text")); updateAll();});
+$("#h_octUp").click(function() {octUp($("#h_seq_text").val(), $("#h_seq_text")); updateAll();});
+$("#s_octDown").click(function() {octDown($("#s_seq_text").val(), $("#s_seq_text")); updateAll();});
+$("#s_octUp").click(function() {octUp($("#s_seq_text").val(), $("#s_seq_text")); updateAll();});
+$("#k_octDown").click(function() {octDown($("#k_seq_text").val(), $("#k_seq_text")); updateAll();});
+$("#k_octUp").click(function() {octUp($("#k_seq_text").val(), $("#k_seq_text")); updateAll();});
+$("#n_octDown").click(function() {octDown($("#n_seq_text").val(), $("#n_seq_text")); updateAll();});
+$("#n_octUp").click(function() {octUp($("#n_seq_text").val(), $("#n_seq_text")); updateAll();});
+
+
+
+//controls for snapshots
+$("#snapshot1_save").click(function() {
+	snapshot1 = snapshot_save();
+});
+$("#snapshot2_save").click(function() {
+	snapshot2 = snapshot_save();
+});
+$("#snapshot3_save").click(function() {
+	snapshot3 = snapshot_save();
+});
+
+$("#snapshot1_set").click(function() {
+	snapshot_set(snapshot1);
+});
+$("#snapshot2_set").click(function() {
+	snapshot_set(snapshot2);
+});
+$("#snapshot3_set").click(function() {
+	snapshot_set(snapshot3);
+});
